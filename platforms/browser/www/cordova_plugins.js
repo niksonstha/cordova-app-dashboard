0be920985aa16be38a2f4822f433ad3808e4b6b5
niksonstha <sthanikson10@gmail.com>
2024-01-04 16:26:07 +0545

    Update dashboard UI based on screen size

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -208,28 +208,13 @@ module.exports = [
         "merges": [
             "window.FileSystem"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
-        "id": "cordova-plugin-device.DeviceProxy",
-        "pluginId": "cordova-plugin-device",
-        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-camera": "7.0.0",
-    "cordova-plugin-file": "8.0.1",
-    "cordova-plugin-device": "2.1.0"
+    "cordova-plugin-file": "8.0.1"
 }
 // BOTTOM OF METADATA
 });
